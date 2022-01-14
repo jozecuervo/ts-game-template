17ab121c5ce5c72988bfe7ef8a8d3d61a6d9613d
José Miguel Hernandez <jose.hernandez@nextroll.com>
2022-01-14 13:06:55 -0800

    Classic "mystify your mind"

--- app/client.ts
+++ app/client.ts
@@ -1,24 +1,39 @@
 import { CANVAS_WIDTH, CANVAS_HEIGHT, FRAME_LENGTH } from 'app/gameConstants';
-import { bindKeyboardListeners } from 'app/utils/keyboard';
-
 
 const mainCanvas = document.getElementById('mainCanvas') as HTMLCanvasElement;
 // `!` here is the "Non-null assertion operator", which we need to use here if we have strict null checks enabled.
 const mainContext = mainCanvas.getContext('2d')!;
 mainContext.imageSmoothingEnabled = false;
 
-// Bind keyboard listeners to keep track of which keys are pressed.
-bindKeyboardListeners();
+const STEP = 6;
 
 // Object to store the game state on.
 const state = {
     time: 0,
     lastRender: -1,
+    xa: 1,
+    ya: 1,
+    xb:0,
+    yb:0,
+    xav:1,
+    yav:2,
+    xbv:-2,
+    ybv:-1,
+    colour: 0,
 };
 
 // Update function advances the state of the game every FRAME_LENGTH milliseconds.
 function update() {
     state.time += FRAME_LENGTH;
+    if (state.xa > CANVAS_WIDTH-10 || state.xa<1) state.xav*=-1;
+    if (state.xb > CANVAS_WIDTH-10 || state.xb<1) state.xbv*=-1;
+    if (state.ya > CANVAS_HEIGHT-10 || state.ya<1) state.yav*=-1;
+    if (state.yb > CANVAS_HEIGHT-10 || state.yb<1) state.ybv*=-1;
+    state.xa += state.xav * STEP;
+    state.xb += state.xbv * STEP;
+    state.ya +=state.yav * STEP;
+    state.yb +=state.ybv * STEP;
+    state.colour++;
 }
 
 // Render will display the current state of the game.
@@ -29,16 +44,18 @@ function render() {
     }
     state.lastRender = state.time;
 
-    // Draw an off-white background:
-    mainContext.fillStyle = '#EEE';
+    // Draw an translucent black background:
+    mainContext.fillStyle = '#0001';
     mainContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    // Draw a green square to the screen that oscillates back and forth over time.
-    mainContext.fillStyle = 'green';
-    const size = 50;
-    const x = CANVAS_WIDTH / 2 - size / 2 + Math.round(50 * Math.sin(state.time / 500));
-    const y = CANVAS_HEIGHT / 2 - size / 2;
-    mainContext.fillRect(x, y, size, size);
+    mainContext.strokeStyle = `rgb(
+        ${state.colour % 255},
+        ${(state.colour * 1.2) % 255},
+        ${(state.colour * 1.7) % 255})`;
+    mainContext.beginPath();
+    mainContext.moveTo(state.xa, state.ya);
+    mainContext.lineTo(state.xb, state.yb);
+    mainContext.stroke();
 }
 
 // Start update loop:
